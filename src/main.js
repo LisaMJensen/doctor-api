004d952c980c6aae6d16394a732bc441aec77428
Lisa Jensen <leese_38@yahoo.com>
2019-07-05 16:03:14 -0700

    add css styling

--- src/main.js
+++ src/main.js
@@ -26,16 +26,27 @@ $(document).ready(function() {
 
           for(let i=0; i<doctorProfile.data.length; i++) {
             $('.showDoctors').append(
-              `<div class="card"><ul><li>${doctorProfile.data[i].profile.first_name}</li>
-              <li>${doctorProfile.data[i].profile.last_name}</li>
+              `<div class="card"><div class="card-title">${doctorProfile.data[i].profile.first_name}
+              ${doctorProfile.data[i].profile.last_name}</div>
+              <table>
+              <tr>
+              <td>
+              <ul>
               <li>${doctorProfile.data[i].practices[0].name}</li>
               <li>${doctorProfile.data[i].practices[0].visit_address.street}<br>${doctorProfile.data[i].practices[0].visit_address.city}, ${doctorProfile.data[i].practices[0].visit_address.state} ${doctorProfile.data[i].practices[0].visit_address.zip}</li>
               <li>${doctorProfile.data[i].practices[0].phones[0].number}</li>
-              <li>${doctorProfile.data[i].practices[0].website}</li>
-              <li>${doctorProfile.data[i].practices[0].accepts_new_patients}</li></ul></div><br>`);
+              <li><a href="${doctorProfile.data[i].practices[0].website}">Website</a></li>
+              <li>${doctorProfile.data[i].practices[0].accepts_new_patients}</li></ul><br>
+              </td>
+              <td>
+              <img id="profilePhoto" src="${doctorProfile.data[i].profile.image_url}">
+              </tr></td>
+              </table>
+              </div><br>`);
 
           }
-        } else { ('.showDoctors').text('No doctors were found in your area that match your search.');}
+        } else { ('.showDoctors').text('No doctors were found in your area that match your search.');
+      }
         })
 
         // if (${doctorProfile.data[i].practices[0].accepts_new_patients} === "true"){
