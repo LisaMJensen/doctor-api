2c93ab84edadb9dfb8d41394bdeb04a71abf149f
Lisa Jensen <leese_38@yahoo.com>
2019-07-05 14:58:09 -0700

    add cards to index where results are displayed

--- src/main.js
+++ src/main.js
@@ -17,8 +17,31 @@ $(document).ready(function() {
         promise.then(function(response) {
 
         let doctorProfile = JSON.parse(response);
-
+        console.log(name);
+        console.log(doctorSearch);
         console.log(doctorProfile);
-      });
+
+        if (doctorProfile.data.length > 0) {
+        $('.showDoctor').empty();
+
+          for(let i=0; i<doctorProfile.data.length; i++) {
+            $('.showDoctors').append(
+              `<div class="card"><ul><li>${doctorProfile.data[i].profile.first_name}</li>
+              <li>${doctorProfile.data[i].profile.last_name}</li>
+              <li>${doctorProfile.data[i].practices[0].name}</li>
+              <li>${doctorProfile.data[i].practices[0].visit_address.street}<br>${doctorProfile.data[i].practices[0].visit_address.city}, ${doctorProfile.data[i].practices[0].visit_address.state} ${doctorProfile.data[i].practices[0].visit_address.zip}</li>
+              <li>${doctorProfile.data[i].practices[0].phones[0].number}</li>
+              <li>${doctorProfile.data[i].practices[0].website}</li>
+              <li>${doctorProfile.data[i].practices[0].accepts_new_patients}</li></ul></div><br>`);
+
+          }
+        } else { ('.showDoctors').text('No doctors were found in your area that match your search.');}
+        })
+
+        // if (${doctorProfile.data[i].practices[0].accepts_new_patients} === "true"){
+        //   return "Yes";
+        // }
+
+
     });
-  });
+    });
