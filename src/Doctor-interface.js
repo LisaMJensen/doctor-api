64f6d8bf249b77dd3c8ddec597ab81dfab334ed9
Lisa Jensen <leese_38@yahoo.com>
2019-07-05 13:51:18 -0700

    separate front and back end logic

--- src/Doctor-interface.js
+++ src/Doctor-interface.js
@@ -1,17 +1,18 @@
-// export class DoctorService {
-//   getDoctorByMedicalIssue(medicalIssue) {
-//     return new Promise(function(resolve, reject) {
-//       let request = new XMLHttpRequest();
-//       const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&3&skip=0&limit=10&user_key=34853a487b63d7e734401c6341ae74f8`;
-//       request.onload = function() {
-//         if (this.status === 200) {
-//           resolve(request.response);
-//         } else {
-//           reject(Error(request.statusText));
-//         }
-//       }
-//       request.open("GET", url, true);
-//       request.send();
-//     });
-//   }
-// }
+export class DoctorService {
+
+  getDoctorByName(name) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://CORS-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&skip=0&limit=10&user_key=34853a487b63d7e734401c6341ae74f8`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,38 +2,23 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import {DoctorService} from './../Doctor-interface.js'
+import {DoctorService} from './Doctor-interface.js'
 
 
 $(document).ready(function() {
-  $('#doctorButton').click(function() {
+  $('#doctorButton').click(function(event) {
+    event.preventDefault();
       let name = $('#enteredName').val();
       $('#enteredName').val("");
 
-      // getDoctorByMedicalIssue(medicalIssue) {
-      //   return new Promise(function(resolve, reject) {
-          let request = new XMLHttpRequest();
-          const url = `https://CORS-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&skip=0&limit=10&user_key=34853a487b63d7e734401c6341ae74f8`;
-          request.onreadystatechange = function() {
-            if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.response);
-        getElements(response);
-        }
-      }
-          request.open("GET", url, true);
-          request.send();
+        let doctorSearch = new DoctorService();
 
-          const getElements = function(response) {
-      if(name == "") {
-        $('.showDoctors').text("No doctors were a match. Please try again.")
-      }
-        $('.showDoctors').append(`"${response.data.profile}"`);
-      // $('.showShiny').html(`<img src="${response.sprites.front_shiny}">`);
-      //   });
-    }
+        let promise = doctorSearch.getDoctorByName(name);
+        promise.then(function(response) {
 
-});
-});
+        let doctorProfile = JSON.parse(response);
 
-
-// `https://CORS-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=WA%2098198&sort=best-match-asc&skip=0&limit=10&user_key=34853a487b63d7e734401c6341ae74f8`;
+        console.log(doctorProfile);
+      });
+    });
+  });
